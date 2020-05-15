af862d47a3a9f304731cb0a0bd807b7c64f23803
Paolo Corti <paolocorti87@gmail.com>
2020-05-15 12:27:19 +0200

    add scrolling feature

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,73 @@
-import React, { Component } from 'react'
-import Timeline from './Timeline'
+import React, { Component } from 'react';
+import Timeline from './Timeline';
 import { Scrollama, Step } from 'react-scrollama';
 
 class Home extends Component {
-
-  onStepEnter = ({ data }) => {
-    console.log(data)
+  state = {
+    data: 0,
+    steps: [10, 20, 30],
+    progress: 0,
   };
 
-  onStepProgress = ({ element, data, progress }) => {
-    console.log(element, data, progress)
+  onStepEnter = ({ element, data }) => {
+    if (element.children[0]) {
+      element.children[0].style.position = 'sticky';
+      element.children[0].style.top = 0;
+    }
+    this.setState({ data });
+  };
+
+  onStepExit = ({ element }) => {
+    if (element.children[0]) {
+      element.children[0].position = 'relative';
+    }
+  };
+
+  onStepProgress = ({ progress }) => {
+    this.setState({ progress });
   };
 
   render() {
+    const { progress, data } = this.state;
     return (
-      <div className="w-100 h-100">
-        <Scrollama onStepEnter={this.onStepEnter} onStepProgress={this.onStepProgress} debug progress>
-          <Step data={0} key={0}>
-            <div className="w-100 h-100 chapter-2" style={{ backgroundColor: 'yellow'}}>
-            </div>
-          </Step>
-          <Step data={1} key={1}>
-            <div className="w-100 h-100 chapter-1">
-              <Timeline />
-            </div>
-          </Step>
-          <Step data={2} key={2}>
-            <div className="w-100 h-100 chapter-2" style={{ backgroundColor: 'red'}}>
-            </div>
-          </Step>
-          <Step data={3} key={3}>
-            <div className="w-100 h-100 chapter-3" style={{ backgroundColor: 'green'}}>
-            </div>
-          </Step>
-        </Scrollama>
+      <div className="w-100">
+        <div
+          className="w-100 chapter-2"
+          style={{ backgroundColor: 'yellow', height: '100vh' }}
+        ></div>
+        <div>
+          <Scrollama
+            onStepEnter={this.onStepEnter}
+            onStepExit={this.onStepExit}
+            progress={true}
+            onStepProgress={this.onStepProgress}
+            debug
+            offset={0}
+          >
+            <Step data={1} key={1}>
+              <div className="w-100 chapter-1" style={{ height: '200vh' }}>
+                <Timeline
+                  progress={data === 1 ? progress : 1}
+                  isScrollActive={data === 1}
+                />
+                {Math.round(progress * 100)}
+              </div>
+            </Step>
+            <Step data={2} key={2}>
+              <div
+                className="w-100 chapter-2"
+                style={{ backgroundColor: 'red', height: '200vh' }}
+              ></div>
+            </Step>
+            <Step data={3} key={3}>
+              <div
+                className="w-100 chapter-3"
+                style={{ backgroundColor: 'green', height: '200vh' }}
+              ></div>
+            </Step>
+          </Scrollama>
+        </div>
       </div>
-      
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import 'tachyons';
-import Home from './Home'
-import Theme from './Theme'
-import Country from './Country'
-import registerServiceWorker from './registerServiceWorker'
+import Home from './Home';
+import Theme from './Theme';
+import Country from './Country';
+import registerServiceWorker from './registerServiceWorker';
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <BrowserRouter>
-        <div className="w-100 h-100">
+        <div className="">
           <Switch>
-            <Route exact path='/' component={Home} />
+            <Route exact path="/" component={Home} />
           </Switch>
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-    <App />
-  , document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
