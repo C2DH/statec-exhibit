7dfa8be06985192403f5cca1956157797f001922
Daniele Guido <gui.daniele@gmail.com>
2020-09-28 11:43:23 +0200

    use build

---
+++ precache-manifest.5d8bc8e34a8be3324b4e1a0aa05121f3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e04281a7c5a7e2aeb924f3a7e2dfe7",
+    "url": "/statec-exhibit/index.html"
+  },
+  {
+    "revision": "3a3fd9a9e80336f9cbfe",
+    "url": "/statec-exhibit/static/css/2.c6861289.chunk.css"
+  },
+  {
+    "revision": "ef25976ea365599e68c7",
+    "url": "/statec-exhibit/static/css/main.903d6580.chunk.css"
+  },
+  {
+    "revision": "3a3fd9a9e80336f9cbfe",
+    "url": "/statec-exhibit/static/js/2.718db898.chunk.js"
+  },
+  {
+    "revision": "365fcd12cf85a52d6576e6413dff9cf1",
+    "url": "/statec-exhibit/static/js/2.718db898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef25976ea365599e68c7",
+    "url": "/statec-exhibit/static/js/main.9f84a5e8.chunk.js"
+  },
+  {
+    "revision": "643e51f7891ecb480a7b",
+    "url": "/statec-exhibit/static/js/runtime-main.8f765d72.js"
+  },
+  {
+    "revision": "2a369affd26b04c1b1025f083b00c006",
+    "url": "/statec-exhibit/static/media/Porpora-Regular.2a369aff.ttf"
+  },
+  {
+    "revision": "574257154a00f2a2d5c8d8182513437d",
+    "url": "/statec-exhibit/static/media/Porpora-Regular.57425715.woff"
+  },
+  {
+    "revision": "a59d56f2dae3590974cf2891b7f31ae1",
+    "url": "/statec-exhibit/static/media/Porpora-Regular.a59d56f2.woff2"
+  },
+  {
+    "revision": "c2c94052ace06f27932b25de506152f4",
+    "url": "/statec-exhibit/static/media/Sneaky-Times.c2c94052.woff"
+  },
+  {
+    "revision": "ceb70cc908334e8c186fc6867cfb5aae",
+    "url": "/statec-exhibit/static/media/Sneaky-Times.ceb70cc9.woff2"
+  },
+  {
+    "revision": "8c215141779af3b12d37621b54b34ce4",
+    "url": "/statec-exhibit/static/media/landing.8c215141.svg"
+  }
+]);
