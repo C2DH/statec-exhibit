8a4b7b6c075f84bfb45be50fd183791c3c71eafa
Daniele Guido <gui.daniele@gmail.com>
2020-11-24 21:47:55 +0100

    add hotspots property when dataset id doesn't change

--- src/components/TrendHotspostsGraphics.js
+++ src/components/TrendHotspostsGraphics.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class TrendHotspotsGraphics extends React.Component{
   shouldComponentUpdate(nextProps) {
-    return this.props.id !== nextProps.id || this.props.windowDimensions !== nextProps.windowDimensions
+    return this.props.id !== nextProps.id || this.props.hotspots !== nextProps.hotspots || this.props.windowDimensions !== nextProps.windowDimensions
   }
 
   render() {
