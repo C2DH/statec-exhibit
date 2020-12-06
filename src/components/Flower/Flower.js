0b5a5d6b5375f6ac05f9660f73dd37c2ce00ed50
danieleguido <gui.daniele@gmail.com>
2020-12-06 15:02:15 +0100

    split flower and its legend

--- src/components/Flower/Flower.js
+++ src/components/Flower/Flower.js
@@ -6,6 +6,7 @@ import moment from 'moment';
 import { red } from '../../constants';
 import { useStore } from '../../store';
 import { isMobileWithTablet, isTabletC } from '../../constants';
+import FlowerLegend from './FlowerLegend'
 
 const nMinPetals = 8;
 
@@ -74,146 +75,110 @@ const Flower = ({
   const progressYear = useStore.getState().actualYear;
 
   return (
-    <div
+    <div className="Flower"
       style={{
         position: 'relative',
         width: dimension,
         margin: isMobileWithTablet ? '0 auto' : 0,
       }}
     >
-      <div className="tc absolute h-50 bl" style={{
-        left: '50%',
-        zIndex:-1,
-      }}><span className="pl2">{minDate}</span></div>
-      <svg width={dimension} height={dimension}>
-        <defs>
-          <linearGradient
-            id={`bezierGradient`}
-            x1="0%"
-            y1="0%"
-            x2="0%"
-            y2="100%"
-            gradientUnits="objectBoundingBox"
-          >
-            <stop offset="0%" stopColor={colorA} stopOpacity={1} />
-            <stop offset="48%" stopColor={colorB} stopOpacity={1} />
-            <stop offset="100%" stopColor={colorC} stopOpacity={1} />
-          </linearGradient>
-        </defs>
-        <g transform={`translate(${dimension / 2},0)`}>
-          <text>{minDate}</text>
-        </g>
-        <g transform={`translate(${dimension / 2},${dimension / 2})`}>
-          <circle cx={0} cy={0} r={radius * 1} stroke={stroke} fill={fill} />
-          {dataArray.map((d, i) => {
-            const isNegative = d.v < 0;
-            const angle = (dataArray.length - i) * angleD + Math.PI;
-            const deg = angle * (180 / Math.PI);
-            const petalWidth =
-              nOfPetals > nMinPetals
-                ? parseInt(circumference / (nOfPetals - 1))
-                : parseInt(circumference / nMinPetals);
-            const curveHeight = scaleYUnclamped(d.v);
-            let selected = false;
-            const keyYear = keysArray[i];
-            if (keyYear.includes('-')) {
-              const [minKey, maxKey] = keyYear.split('-');
-              if (
-                progressYear >= Number(minKey) &&
-                progressYear <= Number(maxKey)
-              ) {
-                selected = true;
-                selectedTime = keyYear;
+      <FlowerLegend title={title} selectedTime={progressYear} />
+      <div className="relative">
+        <div className="tc absolute h-50 bl" style={{
+          left: '50%',
+          zIndex:-1,
+          top: -20
+        }}><span className="pl2">{minDate}</span></div>
+        <svg width={dimension} height={dimension}>
+          <defs>
+            <linearGradient
+              id={`bezierGradient`}
+              x1="0%"
+              y1="0%"
+              x2="0%"
+              y2="100%"
+              gradientUnits="objectBoundingBox"
+            >
+              <stop offset="0%" stopColor={colorA} stopOpacity={1} />
+              <stop offset="48%" stopColor={colorB} stopOpacity={1} />
+              <stop offset="100%" stopColor={colorC} stopOpacity={1} />
+            </linearGradient>
+          </defs>
+          <g transform={`translate(${dimension / 2},0)`}>
+            <text>{minDate}</text>
+          </g>
+          <g transform={`translate(${dimension / 2},${dimension / 2})`}>
+            <circle cx={0} cy={0} r={radius * 1} stroke={stroke} fill={fill} />
+            {dataArray.map((d, i) => {
+              const isNegative = d.v < 0;
+              const angle = (dataArray.length - i) * angleD + Math.PI;
+              const deg = angle * (180 / Math.PI);
+              const petalWidth =
+                nOfPetals > nMinPetals
+                  ? parseInt(circumference / (nOfPetals - 1))
+                  : parseInt(circumference / nMinPetals);
+              const curveHeight = scaleYUnclamped(d.v);
+              let selected = false;
+              const keyYear = keysArray[i];
+              if (keyYear.includes('-')) {
+                const [minKey, maxKey] = keyYear.split('-');
+                if (
+                  progressYear >= Number(minKey) &&
+                  progressYear <= Number(maxKey)
+                ) {
+                  selected = true;
+                  selectedTime = keyYear;
+                }
+              } else {
+                if (Number(keyYear) === progressYear) {
+                  selected = true;
+                  selectedTime = keyYear;
+                }
               }
-            } else {
-              if (Number(keyYear) === progressYear) {
-                selected = true;
-                selectedTime = keyYear;
+
+              if (selected) {
+                console.log((dataArray.length - i) * angleD);
               }
-            }
 
-            if (selected) {
-              console.log((dataArray.length - i) * angleD);
-            }
-
-            return (
-              <g
-                transform={`translate(${Math.sin(angle) * radius}, ${
-                  Math.cos(angle) * radius
-                }) `}
-                key={`petal-${i}`}
-              >
+              return (
                 <g
-                  transform={`translate(-${petalWidth / 2},${
-                    curveHeight * -1
-                  }) rotate(${-deg + 180}, ${petalWidth / 2}, ${curveHeight})`}
+                  transform={`translate(${Math.sin(angle) * radius}, ${
+                    Math.cos(angle) * radius
+                  }) `}
+                  key={`petal-${i}`}
                 >
-                  <Bezier
-                    height={curveHeight}
-                    width={petalWidth}
-                    c1={petalWidth * 0.2}
-                    c2={petalWidth * 0.1}
-                    selected={selected}
-                  />
+                  <g
+                    transform={`translate(-${petalWidth / 2},${
+                      curveHeight * -1
+                    }) rotate(${-deg + 180}, ${petalWidth / 2}, ${curveHeight})`}
+                  >
+                    <Bezier
+                      height={curveHeight}
+                      width={petalWidth}
+                      c1={petalWidth * 0.2}
+                      c2={petalWidth * 0.1}
+                      selected={selected}
+                    />
+                  </g>
+                  <text
+                    style={{
+                      display: selected ? 'block' : 'none',
+                      textAnchor: 'middle',
+                      transform: `rotate(${-deg})`,
+                      dominantBaseline: 'central',
+                      color: '#2b219f',
+                      fontSize: '12px',
+                    }}
+                    dx={Math.sin(angle) * radius * 0.5}
+                    dy={Math.cos(angle) * radius * 0.5}
+                  >
+                    {d.v}
+                  </text>
                 </g>
-                <text
-                  style={{
-                    display: selected ? 'block' : 'none',
-                    textAnchor: 'middle',
-                    transform: `rotate(${-deg})`,
-                    dominantBaseline: 'central',
-                    color: '#2b219f',
-                    fontSize: '12px',
-                  }}
-                  dx={Math.sin(angle) * radius * 0.5}
-                  dy={Math.cos(angle) * radius * 0.5}
-                >
-                  {d.v}
-                </text>
-              </g>
-            );
-          })}
-        </g>
-      </svg>
-      <div
-        dx={2}
-        style={{
-          color: '#2b219f',
-          fontFamily: 'Sneaky',
-          fontSize: isMobileWithTablet
-            ? isTabletC
-              ? '24px'
-              : '14px'
-            : '1.8vh',
-          justifyContent: 'center',
-          textAlign: 'center',
-          position: 'absolute',
-          // top: isMobileWithTablet ? '0' : '0',
-          // width: isMobileWithTablet ? '100%' : '100%',
-          bottom: 0,
-          left: 20,
-          right: 20,
-          marginTop: '0px',
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        {title} - {selectedTime}
-        <br />
-        <div
-          style={{
-            fontFamily: 'Porpora, sans-serif',
-            fontWeight: 'normal',
-            fontSize: '12px',
-            lineHeight: '14px',
-            marginTop: '5px',
-            color: 'rgba(0, 0, 0, 0.5)',
-          }}
-        >
-          Length of the petal = data in the indicated years
-        </div>
-        <br></br>
+              );
+            })}
+          </g>
+        </svg>
       </div>
     </div>
   );
