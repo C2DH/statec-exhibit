f1e1831a1a98fd12a90577661f251a9a5c84bbf8
danieleguido <gui.daniele@gmail.com>
2021-02-15 08:33:12 +0100

    eslint

--- src/components/Chapter/ChapterFooter.js
+++ src/components/Chapter/ChapterFooter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { ChapterRoutesWithIndex, isMobileWithTablet } from '../../constants'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import styles from '../../pages/Home.module.css';
 
 
