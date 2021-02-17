471645c80e53692158eaf4bb6b962658ea438921
Daniele Guido <gui.daniele@gmail.com>
2021-02-17 18:07:48 +0100

    add caption

--- src/components/MediaImage.js
+++ src/components/MediaImage.js
@@ -5,7 +5,13 @@ import { isMobileWithTablet } from '../constants';
 import { useImage } from '../hooks';
 
 
-const MediaImage = ({ id, to, alt, caption, title = '', preview, height = '40vh', padding, resolution = 'medium-h' }) => {
+const MediaImage = ({
+  id, to, alt, caption = '', title = '',
+  preview,
+  height = '45vh', padding = 1,
+  resolution = 'medium-h',
+  displayTitle = true,
+}) => {
   const media = MediaIndex.images[id]
   const mediaUrl = media
     ? media.resolutions[resolution].url
@@ -13,9 +19,14 @@ const MediaImage = ({ id, to, alt, caption, title = '', preview, height = '40vh'
   const mediaBase64 = media?.base64
   const { isLoading } = useImage(mediaUrl, 50);
   const backgroundImage = `url(${isLoading ? mediaBase64 : mediaUrl})`
-
-  console.info('MediaImage rendered', id, backgroundImage, mediaUrl)
-
+  const mediaCaption = [
+    { text: caption, className: 'MediaImage-narrative'},
+    { text: media?.caption, className: 'MediaImage-caption mt2' },
+    { text: media?.provenance, className:'MediaImage-provenance mt2' },
+  ].filter(({text}) => typeof text === 'string' && text.length)
+    .map(({text, className=''}) => `<div class="${className}">${text}</div>`)
+    .join('')
+  
   return (
     <figure
       style={{
@@ -26,14 +37,14 @@ const MediaImage = ({ id, to, alt, caption, title = '', preview, height = '40vh'
         margin: '0 auto',
       }}
     >
-      {!!title.length && <h2 className="textContainerTitle">{title}</h2>}
+      {displayTitle && !!title.length && <h2 className="textContainerTitle">{title}</h2>}
       <div style={{ height: padding, backgroundColor: 'black' }}></div>
       <div
         style={{
           backgroundImage,
           backgroundColor: 'black',
           transition: '0.16s opacity ease-in',
-          opacity: `${isLoading ? 0.2 : 1}`,
+          // opacity: `${isLoading ? 0.2 : 1}`,
           backgroundSize: 'contain',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
@@ -41,6 +52,8 @@ const MediaImage = ({ id, to, alt, caption, title = '', preview, height = '40vh'
           overflow: 'hidden',
           borderLeft: `${padding}px solid`,
           borderRight: `${padding}px solid`,
+          borderTop: `${padding}px solid black`,
+          borderBottom: `${padding}px solid black`,
           position: 'relative',
         }}
       >
@@ -63,7 +76,7 @@ const MediaImage = ({ id, to, alt, caption, title = '', preview, height = '40vh'
       </div>
       <div style={{ height: padding, backgroundColor: 'black' }}></div>
       <figcaption
-        dangerouslySetInnerHTML={{ __html: caption }}
+        dangerouslySetInnerHTML={{ __html: mediaCaption }}
         style={{
           padding,
           flexShrink: '1',
