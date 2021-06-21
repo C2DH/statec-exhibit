a4059c230c164c8136d8456394e84edb027f08f9
Daniele Guido <gui.daniele@gmail.com>
2021-06-21 15:01:19 +0200

    leave intact the hash

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,14 +17,20 @@ const Header = () => {
       backgroundColor,
     }}>
       <div className="Header_sideLinkWrapper w-100 flex items-center justify-space-between">
-      <div className="Header_sideLink" onClick={() => history.push({ search: '?panel=table-of-contents' })}>
+      <div className="Header_sideLink" onClick={() => history.push({
+        search: '?panel=table-of-contents',
+        hash: window.location.hash.replace('#', ''),
+      })}>
         <span className="db dn-ns">{t('tableOfContentsMobile')}</span>
         <span className="dn db-ns">{t('tableOfContents')}</span>
       </div>
       <div>
         <Link to={'/'}>{t('FramingLuxembourg')}</Link>
       </div>
-      <div className="Header_sideLink tr" onClick={() => history.push({ search: '?panel=about' })}>
+      <div className="Header_sideLink tr" onClick={() => history.push({
+        search: '?panel=about',
+        hash: window.location.hash.replace('#', '')
+      })}>
         {t('AboutTitle')}
       </div>
       </div>
