6f2f93ffc22183d0d7f5c2759c2bc84d249d5b2a
Daniele Guido <gui.daniele@gmail.com>
2021-07-05 12:20:56 +0200

    add hook to get bounding client rect useBoundingClientRect

--- src/hooks.js
+++ src/hooks.js
@@ -140,3 +140,39 @@ export function useMousePosition() {
 export const useURLSearchParams = () => {
   return new URLSearchParams(useLocation().search);
 }
+
+/**
+ * Calculate available rectangle for the given ref.
+ * usage inside functional components:
+ *
+ *     const [bbox, ref] = useBoundingClientRect()
+ *     return (<div ref="ref"></div>)
+ */
+export const useBoundingClientRect = () => {
+  const ref = useRef();
+  const [bbox, setBbox] = useState({
+    width: 0, height: 0, windowDimensions: '0x0'
+  });
+  const setCurrentBoundingClientRect = () => {
+    const boundingClientRect = ref && ref.current
+      ? ref.current.getBoundingClientRect()
+      : { width: 0, height: 0, windowDimensions: '0x0' }
+    const windowDimensions = `${boundingClientRect.width}x${boundingClientRect.height}`
+    if (windowDimensions !== bbox.windowDimensions) {
+      // extract one dimension by one dimension, the only way
+      // as the result of el.getBoundingClientRect() returns a special object
+      // of type ClientRect (or DomRect apparently)
+      const {top, right, bottom, left, width, height, x, y} = boundingClientRect
+      setBbox({
+        top, right, bottom, left, width, height, x, y,
+        windowDimensions,
+      })
+    }
+  };
+  useEffect(() => {
+    setCurrentBoundingClientRect()
+    window.addEventListener('resize', setCurrentBoundingClientRect);
+    return () => window.removeEventListener('resize', setCurrentBoundingClientRect);
+  });
+  return [bbox, ref];
+};
