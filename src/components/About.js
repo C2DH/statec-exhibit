3196976294ce58e2254f0231c9fd7f86f89e2bc2
Daniele Guido <gui.daniele@gmail.com>
2022-01-20 14:52:30 +0100

    fix h2 and h3 element in about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,12 @@ import { useTranslation } from 'react-i18next'
 const About = ({ color='white' }) => {
   const { t } = useTranslation()
   return (
-    <div className="About" style={{color}}>
+    <div className="About mb5" style={{color}}>
       <h1 className="ma0 pa0 mb4 f3 f2-m f1-l" style={{color}}>{t('AboutTitle')}</h1>
       <section className="f5 f4-m f3-l lh-copy">
-      <p dangerouslySetInnerHTML={{ __html: t('AboutIntroduction')}} />
-      <p dangerouslySetInnerHTML={{ __html: t('AboutConcept')}} />
-      <p dangerouslySetInnerHTML={{ __html: t('AboutDevelopment')}} />
+      <div dangerouslySetInnerHTML={{ __html: t('AboutIntroduction')}} />
+      <div dangerouslySetInnerHTML={{ __html: t('AboutConcept')}} />
+      <div dangerouslySetInnerHTML={{ __html: t('AboutDevelopment')}} />
       </section>
     </div>
   )
